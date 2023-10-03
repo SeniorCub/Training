dbbcd1797f34ef46693f0785d2e583b1fb7b2a55
SeniorCub <farinderif@gmail.com>
2023-10-03 01:37:20 +0100

    creating a Chat Interface

---
+++ Chat/script.js
@@ -0,0 +1,9 @@
+// // Get a reference to the toggle button and the emoji tray
+// const toggleButton = document.getElementById('toggleEmojiTray');
+// const emojiTray = document.querySelector('.emoji-tray');
+
+// // Add a click event listener to the toggle button
+// toggleButton.addEventListener('click', () => {
+//     // Toggle the visibility of the emoji tray by adding/removing a 'visible' class
+//     emojiTray.classList.toggle('visible');
+// });
