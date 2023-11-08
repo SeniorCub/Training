a4f46169b050a092999b45662ae6e07f10c1a087
SeniorCub <farinderif@gmail.com>
2023-11-08 21:54:40 +0100

    Started cloning a site

---
+++ JavaScript Video/JS1/script/script.js
@@ -0,0 +1,14 @@
+let grade;
+if (testScore >= 90) {
+     grade = "A";
+} else if (testScore >= 80){
+     grade = "B";
+} else if (testScore >= 80){
+     grade = "B";
+} else if (testScore >= 80){
+     grade = "B";
+} else if (testScore >= 80){
+     grade = "B";
+} else if (testScore >= 80){
+     grade = "B";
+}~
