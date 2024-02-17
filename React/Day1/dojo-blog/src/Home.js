d378a2221487b7dbaa056461c7b7098c9dca4807
seniorcub <farinderif@gmail.com>
2024-02-17 13:17:46 +0100

    Done with Lesson 7 Click Events

--- React/Day1/dojo-blog/src/Home.js
+++ React/Day1/dojo-blog/src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+     const handleClick = (e) => {
+          console.log('hello, ninjas', e);
+     }
+     const handleClickAgain =(name, e) => {
+          console.log('hello ' + name, e.target);
+     }
+
      return (
           <div className="home">
                <h2>Home Page</h2>
+               <button onClick={handleClick}> Click me </button>
+               <button onClick={(e)=> handleClickAgain('mario', e)}>Click me again</button>
           </div>
      );
 }
