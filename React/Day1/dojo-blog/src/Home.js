b9a7c6db3441bb450a805ee945d43ac228f29bf9
seniorcub <farinderif@gmail.com>
2024-02-20 14:40:53 +0100

    done with lesson 8 (Using State :-: useState hook)

--- React/Day1/dojo-blog/src/Home.js
+++ React/Day1/dojo-blog/src/Home.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+     // let name = 'mario';
+     const [name, setName] = useState('mario');
+     const [age, setAge] = useState(25);
 
      const handleClick = (e) => {
-          console.log('hello, ninjas', e);
-     }
-     const handleClickAgain =(name, e) => {
-          console.log('hello ' + name, e.target);
+          setName('luigi');
+          setAge(30)
      }
 
      return (
           <div className="home">
                <h2>Home Page</h2>
+               <p>{ name } is { age } years old</p>
                <button onClick={handleClick}> Click me </button>
-               <button onClick={(e)=> handleClickAgain('mario', e)}>Click me again</button>
           </div>
      );
 }
